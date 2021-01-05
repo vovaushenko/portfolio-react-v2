18e0b65ccb7e8e8877248f44e85833fdb5080928
Vova Ushenko <71892721+vovaushenko@users.noreply.github.com>
2021-01-05 20:29:36 +0200

    Sidebar initial commit

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,7 +1,36 @@
 import React from 'react';
+import {
+    CloseIcon,
+    Icon,
+    SidebarContainer,
+    SidebarLink,
+    SidebarMenu,
+    SidebarRoute,
+    SidebarWrapper,
+    SideBtnWrap,
+} from './SidebarElements';
 
 const Sidebar = () => {
-    return <div></div>;
+    return (
+        <SidebarContainer>
+            <Icon>
+                <CloseIcon />
+            </Icon>
+
+            <SidebarWrapper>
+                <SidebarMenu>
+                    <SidebarLink to="about">About</SidebarLink>
+                    <SidebarLink to="discover">Portfolio</SidebarLink>
+                    <SidebarLink to="services">Skills</SidebarLink>
+                    <SidebarLink to="signup">Contact</SidebarLink>
+                </SidebarMenu>
+
+                <SideBtnWrap>
+                    <SidebarRoute to="/contact">Contact</SidebarRoute>
+                </SideBtnWrap>
+            </SidebarWrapper>
+        </SidebarContainer>
+    );
 };
 
 export default Sidebar;
