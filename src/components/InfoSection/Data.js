feb0eaa81fc06e596aeb9fd38f5c6fc1e32dc4b5
Vova Ushenko <71892721+vovaushenko@users.noreply.github.com>
2021-01-06 20:04:19 +0200

    Info Sections update

--- src/components/InfoSection/Data.js
+++ src/components/InfoSection/Data.js
@@ -1,3 +1,7 @@
+import img1 from '../../images/svg-1.svg';
+import img2 from '../../images/svg-2.svg';
+import img3 from '../../images/svg-3.svg';
+
 export const homeObjOne = {
     id: 'about',
     lightBg: false,
@@ -9,9 +13,45 @@ export const homeObjOne = {
         'By using ReactJS ⚛️, I have made a clone of this amazing app. It actually works, so you can search whathever you want.',
     buttonLabel: 'Watch Online',
     imgStart: false,
-    img: require('../../images/svg-1.svg'),
+    img: img1,
     alt: 'Google Search',
     dark: true,
     primary: true,
     darkText: false,
+};
+
+export const homeObjTwo = {
+    id: 'discover',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'LinkedIn Clone',
+    headline: 'Upgraded design of the most commonly used search engine',
+    description:
+        'By using ReactJS ⚛️, I have made a clone of this amazing app. It actually works, so you can search whathever you want.',
+    buttonLabel: 'Watch Online',
+    imgStart: true,
+    img: img2,
+    alt: 'LinkedIn',
+    dark: false,
+    primary: false,
+    darkText: true,
+};
+
+export const homeObjThree = {
+    id: 'signup',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Google Search Clone',
+    headline: 'Upgraded design of the most commonly used search engine',
+    description:
+        'By using ReactJS ⚛️, I have made a clone of this amazing app. It actually works, so you can search whathever you want.',
+    buttonLabel: 'Watch Online',
+    imgStart: false,
+    img: img3,
+    alt: 'Google Search',
+    dark: false,
+    primary: false,
+    darkText: true,
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Sidebar from '../components/Sidebar';
 import Navbar from '../components/Navbar';
 import HeroSection from '../components/HeroSection';
 import InfoSection from '../components/InfoSection';
-import { homeObjOne } from '../components/InfoSection/Data';
+import { homeObjOne, homeObjThree, homeObjTwo } from '../components/InfoSection/Data';
 
 const Home = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -18,6 +18,8 @@ const Home = () => {
             <Navbar toggle={toggle} />
             <HeroSection />
             <InfoSection {...homeObjOne} />
+            <InfoSection {...homeObjTwo} />
+            <InfoSection {...homeObjThree} />
         </>
     );
 };
