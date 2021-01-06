ec0a9e1f43e881f85aad1df08af5b839169ccc14
Vova Ushenko <71892721+vovaushenko@users.noreply.github.com>
2021-01-06 19:26:38 +0200

    Added Data && Images

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,17 @@
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Google Search Clone',
+    headline: 'Upgraded design of the most commonly used search engine',
+    description:
+        'By using ReactJS ⚛️, I have made a clone of this amazing app. It actually works, so you can search whathever you want.',
+    buttonLabel: 'Watch Online',
+    imgStart: false,
+    img: require('../../images/svg-1.svg'),
+    alt: 'Google Search',
+    dark: true,
+    primary: true,
+    darkText: false,
+};
