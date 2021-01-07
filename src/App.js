9032403ae8f005f8bbaa6c69869d9de3d8da6330
Vova Ushenko <71892721+vovaushenko@users.noreply.github.com>
2021-01-07 16:30:33 +0200

    Added Footer boilerPlate

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages';
+import SigninPage from './pages/signin';
 
 function App() {
     return (
         <Router>
-            <Home />
+            <Switch>
+                <Route path="/" component={Home} exact />
+                <Route path="/contact" component={SigninPage} exact />
+            </Switch>
         </Router>
     );
 }
---
+++ src/pages/signin.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SigninPage = () => {
+    return (
+        <div>
+            <h1>Contact Page</h1>
+        </div>
+    );
+};
+
+export default SigninPage;
