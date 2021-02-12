a72a954cfb917cc6e272dc1595efb390e383aeaa
Vova Ushenko <71892721+vovaushenko@users.noreply.github.com>
2021-02-12 16:22:23 +0200

    form update && tech el update

--- src/components/Technologies/TechnologiesElements.js
+++ src/components/Technologies/TechnologiesElements.js
@@ -3,80 +3,81 @@ import styled from 'styled-components';
 // ************   Tech Container   ***************
 
 export const TechContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    max-width: 1280px;
-    color: #fff;
-    margin: auto;
-    padding: 50px 0;
-    background: ${({ lightMode }) => (lightMode ? '#f9f9f9' : '#010606')};
-    justify-content: center;
-    align-items: center;
-    @media screen and (max-width: 768px) {
-        padding: 40px 0;
-    }
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 50px 0;
+
+  @media screen and (max-width: 768px) {
+    padding: 40px 0;
+  }
 `;
 
 // *****************Tech Header*******************
 
 export const TechHeader = styled.h2`
-    color: ${({ lightMode }) => (lightMode ? '#010606' : '#01bf71')};
+  color: ${({ lightMode }) => (lightMode ? '#010606' : '#01bf71')};
+  font-family: 'Poppins', sans-serif;
+  font-size: 20px;
+  line-height: 30px;
+  font-weight: 500;
+  letter-spacing: 1.1px;
+  text-align: center;
+  margin-top: 16px;
+  padding: 10px;
 
-    font-size: 20px;
-    line-height: 30px;
-    font-weight: 500;
-    letter-spacing: 1.2px;
-    text-transform: uppercase;
-    text-align: center;
-    margin-top: 16px;
-    padding: 20px;
-
-    @media screen and (max-width: 500px) {
-        font-size: 16px;
-    }
+  @media screen and (max-width: 500px) {
+    font-size: 16px;
+  }
 `;
 
 //************Tech  Wrapper******************
 
 export const TechWrapper = styled.div`
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-    max-width: 900px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  max-width: 800px;
 `;
 
 //************TechWrapper******************
 
-export const TechSubHeader = styled.h4`
-    letter-spacing: 1.5px;
-    font-weight: 500;
-    font-family: 'Poppins', sans-serif;
+export const TechSubHeader = styled.p`
+  font-family: 'Poppins', sans-serif;
+  letter-spacing: 1.1px;
+  @media screen and (max-width: 500px) {
+    font-size: 12px;
+  }
 `;
 
 //  *********** Tech Card *******************
 export const TechCard = styled.div`
-    background: #010606;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    border-radius: 10px;
-    padding: 30px;
-    transition: all 0.2s ease-in-out;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-radius: 10px;
+  padding: 30px;
+  transition: all 0.4s ease-in-out;
 
-    &:hover {
-        transform: translateY(-15px);
-        transition: all 0.2s ease-in-out;
-    }
+  &:hover {
+    transform: translateY(-15px);
+    transition: all 0.4s ease-in-out;
+  }
 `;
 
 // *********** Tech Icon *******************
 export const TechIcon = styled.img`
-    height: 60px;
-    width: 60px;
-    margin-bottom: 10px;
+  width: 60px;
+  margin-bottom: 5px;
 
-    &:hover {
-        filter: brightness(130%);
-    }
+  &:hover {
+    filter: brightness(130%);
+  }
+  @media screen and (max-width: 500px) {
+    width: 50px;
+  }
 `;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,42 +1,61 @@
 import React, { useState } from 'react';
+import { createGlobalStyle } from 'styled-components';
 import Sidebar from '../components/Sidebar';
 import Navbar from '../components/Navbar';
 import HeroSection from '../components/HeroSection';
 import ContactForm from '../components/ContactForm';
-
 import {
-    projectFour,
-    projectOne,
-    projectThree,
-    projectTwo,
+  projectFour,
+  projectOne,
+  projectThree,
+  projectTwo,
 } from '../components/Project/Data';
-
 import Tech from '../components/Technologies';
 import Project from '../components/Project';
 import Footer from '../components/Footer';
 
+const GlobalStyle = createGlobalStyle`
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Russo One', sans-serif;
+}
+p,input,textarea {
+    font-family: 'Poppins', sans-serif;
+}
+
+ 
+ body {
+    font-family: 'Poppins', sans-serif;
+    background:#010606;
+    color:#fff
+ }
+ `;
+
 const Home = () => {
-    const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-    const toggle = () => {
-        setIsOpen(!isOpen);
-    };
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
 
-    return (
-        <>
-            <Sidebar isOpen={isOpen} toggle={toggle} />
-            <Navbar toggle={toggle} />
-            <HeroSection />
-            <Tech />
-            <Project {...projectOne} />
-            <Project {...projectTwo} />
-            <Project {...projectThree} />
-            <Project {...projectFour} />
-            <ContactForm />
+  return (
+    <>
+      <GlobalStyle />
+      <Sidebar isOpen={isOpen} toggle={toggle} />
+      <Navbar toggle={toggle} />
+      <HeroSection />
+      <Tech />
+      <Project {...projectOne} />
+      <Project {...projectTwo} />
+      <Project {...projectThree} />
+      <Project {...projectFour} />
+      <ContactForm />
 
-            <Footer />
-        </>
-    );
+      <Footer />
+    </>
+  );
 };
 
 export default Home;
