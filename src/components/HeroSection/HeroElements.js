3e7beaabe1e141a1f7d5b209b08a4a7c4a4c9fec
Vova Ushenko <71892721+vovaushenko@users.noreply.github.com>
2021-02-23 20:21:06 +0200

    Fixed height

--- src/components/HeroSection/HeroElements.js
+++ src/components/HeroSection/HeroElements.js
@@ -3,12 +3,11 @@ import { MdKeyboardArrowRight, MdArrowForward } from 'react-icons/md';
 
 // ***********Hero Container**************
 export const HeroContainer = styled.div`
-  background: #0c0c0c;
   display: flex;
   justify-content: center;
   align-items: center;
   padding: 0 30px;
-  height: 1000px;
+  height: 100vh;
   position: relative;
   z-index: 1;
 `;
